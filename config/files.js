90692f5acb0870d4eedae0bd73ccf2349fe44bfb
Jim Anders <github@janders223.com>
2014-05-28 14:20:58 -0400

    Merge changes from design branch

--- config/files.js
+++ config/files.js
@@ -6,21 +6,33 @@
  *   - https://github.com/linemanjs/lineman/blob/master/config/files.coffee
  */
 module.exports = function(lineman) {
-  //Override file patterns here
-  return {
-    js: {
-      vendor: [
-        "vendor/js/underscore.js",
-        "vendor/js/mediaelement-and-player.min.js",
-        "vendor/bower/angular/angular.js",
-        "vendor/bower/crossroadsMenu/dist/crossroadsMenu.js",
-        "vendor/bower/masonry/dist/masonry.pkgd.js",
-        "vendor/js/**/*.js"
-      ],
-      app: [
-        "app/js/app.js",
-        "app/js/**/*.js"
-      ]
-    }
-  };
+    //Override file patterns here
+    return {
+	js: {
+	    vendor: [
+		"vendor/js/underscore.js",
+		"vendor/js/mediaelement-and-player.min.js",
+		"vendor/bower/angular/angular.js",
+		"vendor/bower/crossroadsMenu/dist/crossroadsMenu.js",
+		"vendor/bower/masonry/dist/masonry.pkgd.js",
+		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/affix.js",
+		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/alert.js",
+		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/button.js",
+		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/carousel.js",
+		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js",
+	  	"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/dropdown.js",
+	  	"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tab.js",
+	  	"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/transition.js",
+		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/scrollspy.js",
+	  	"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js",
+	  	"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tooltip.js",
+	  	"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/popover.js",
+		"vendor/js/**/*.js"
+	    ],
+	    app: [
+		"app/js/app.js",
+		"app/js/**/*.js"
+	    ]
+	}
+    };
 };
