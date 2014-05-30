d55c99d82713dc9c301644f61b729002d5f74c3b
Jim Anders <github@janders223.com>
2014-05-30 16:01:37 -0400

    Finalize oauth login

--- config/files.js
+++ config/files.js
@@ -15,6 +15,8 @@ module.exports = function(lineman) {
 		"vendor/bower/angular/angular.js",
 		"vendor/bower/angular-strap/dist/angular-strap.js",
 		"vendor/bower/angular-strap/dist/angular-strap.tpl.js",
+		"vendor/bower/angular-cookies/angular-cookies.js",
+		"vendor/bower/crdsAuth/dist/crdsAuth.js",
 		"vendor/bower/crossroadsMenu/dist/crossroadsMenu.js",
 		"vendor/bower/masonry/dist/masonry.pkgd.js",
 		"vendor/bower/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/affix.js",
