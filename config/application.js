db6e46295a1fdb2200d832adc6c6277337b39fc9
Jim Anders <github@janders223.com>
2014-06-03 11:18:42 -0400

    Add basic node server to serve static files
    Attempting to use a new buildpack to run static assets from nod.js
    
    Apparently multi buildpacks need the https github address
    
    Change buildpacks
    
    Stop the grunt-concurrent complaint
    
    Change bower tasks
    
    Try again to remove bower
    
    One  more again
    
    Move coffeescript to not be a dev dep
    
    also lineman bower should be prod dep
    
    Move other non dev deps
    
    TRy taking out deps back to dev deps
    
    Don't use coffeescript for nod server
    
    Fix nod server from being on fixed port
    
    Only need one dot in there

--- config/application.js
+++ config/application.js
@@ -12,87 +12,82 @@
  *   $ lineman config concat.js #=> to see the JS config for the concat task.
  */
 module.exports = function(lineman) {
-  //Override application configuration here. Common examples follow in the comments.
-  var app = lineman.config.application;
-  return {
-    // grunt-angular-templates assumes your module is named "app", but
-    // you can override it like so:
-    //
-     ngtemplates: {
-       options: {
-           module: "crossroads"
-       }
-     },
+    //Override application configuration here. Common examples follow in the comments.
+    var app = lineman.config.application;
+    return {
+	// grunt-angular-templates assumes your module is named "app", but
+	// you can override it like so:
+	//
+	ngtemplates: {
+	    options: {
+		module: "crossroads"
+	    }
+	},
+	loadNpmTasks: app.loadNpmTasks.concat("grunt-jekyll", "grunt-nodemon"),
+	prependTasks: {
+	    common: ["jekyll"].concat(app.prependTasks.common)
+	},
+	removeTasks: {
+	    dev: ["server"]
+	},
 
-    loadNpmTasks: app.loadNpmTasks.concat("grunt-jekyll"),
-    prependTasks: {
-      common: ["jekyll"].concat(app.prependTasks.common)
-    },
-    // Sass
-    //
-    // Lineman supports Sass via grunt-contrib-sass, which requires you first
-    // have Ruby installed as well as the `sass` gem. To enable it, comment out the
-    // following line:
-    //
-    enableSass: true,
-    //ACTION: need to update this to generate a minified version once dev is complete
-    sass: {
-      compile: {
-        options: {
-          noCache: true
-        },
-      }
-    },
+	// grunt-angular-templates assumes your module is named "app", but
+	// you can override it like so:
+	//
+	// ngtemplates: {
+	//   options: {
+	//     module: "myModuleName"
+	//   }
+	// }
+	// Sass
+	//
+	// Lineman supports Sass via grunt-contrib-sass, which requires you first
+	// have Ruby installed as well as the `sass` gem. To enable it, comment out the
+	// following line:
+	//
+	enableSass: true,
+	//ACTION: need to update this to generate a minified version once dev is complete
+	sass: {
+	    compile: {
+		options: {
+		    noCache: true
+		},
+	    }
+	},
+	jekyll: {
+	    build: {
+		options: {
+		    src: 'app',
+		    dest: 'generated',
+		    bundleExec: true,
+		    config: 'config/_config.yml'
+		}
+	    }
+	},
+	watch: {
+	    jekyll: {
+		files: [
+		    "app/**/*.md",
+		    "app/**/*.markdown",
+		    "app/**/*.html",
+		    "app/_data/*.yml"
+		],
+		tasks: ["jekyll:build", "sass:compile", "coffee", "concat_sourcemap"]
+	    },
+	    scss: {
+		files: 'app/css/*.scss',
+		tasks: ['sass:compile']
+	    }
+	},
+	nodemon: {
+	    dev: {
+		script: 'server/server.js',
+		ext: 'js,coffee',
+		watch: ['server'],
+		delay: 1000,
+		legacyWatch: true
+	    }
+	}
 
-
-
-    server: {
-      pushState: true
-      // API Proxying
-      //
-      // During development, you'll likely want to make XHR (AJAX) requests to an API on the same
-      // port as your lineman development server. By enabling the API proxy and setting the port, all
-      // requests for paths that don't match a static asset in ./generated will be forwarded to
-      // whatever service might be running on the specified port.
-      //
-      // apiProxy: {
-      //   enabled: true,
-      //   host: 'localhost',
-      //   port: 3000
-      // }
-    },
-    jekyll: {
-      build: {
-        options: {
-          src: 'app',
-          dest: 'generated',
-          bundleExec: true,
-          config: 'config/_config.yml'
-        }
-      }
-    },
-    watch: {
-      jekyll: {
-        files: [
-          "app/**/*.md",
-          "app/**/*.markdown",
-          "app/**/*.html",
-          "app/_data/*.yml"
-        ],
-        tasks: ["jekyll:build", "sass:compile", "coffee", "concat_sourcemap"],
-      },
-      scss: {
-        files: 'app/css/*.scss',
-        tasks: ['sass:compile']
-      },
-    }
-    // Asset Fingerprints
-    //
-    // Lineman can fingerprint your static assets by appending a hash to the filename
-    // and logging a manifest of logical-to-hashed filenames in dist/assets.json
-    // via grunt-asset-fingerprint
-    //
-    // enableAssetFingerprint: true
-
-  };
+    };
 };
