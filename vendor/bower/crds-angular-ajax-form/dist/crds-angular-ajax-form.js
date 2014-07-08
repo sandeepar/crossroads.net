1f7ab243f9abe83560bff163f30db925400f59c8
Chris Tallent <chris@tallented.net>
2014-07-08 08:27:54 -0400

    MIL-297: Add contact form success messages

--- vendor/bower/crds-angular-ajax-form/dist/crds-angular-ajax-form.js
+++ vendor/bower/crds-angular-ajax-form/dist/crds-angular-ajax-form.js
@@ -31,19 +31,172 @@
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty;
 
-  angular.module("crds-ajax-form").directive("crdsAjaxForm", function($compile) {
+  angular.module("crds-ajax-form").directive("crdsAjaxForm", function($timeout) {
     return {
       restrict: 'A',
       scope: true,
+      require: ['crdsAjaxForm', 'form'],
+      link: function($scope, $element, $attrs, controllers) {
+        return new ((function() {
+          _Class.elements = null;
+
+          _Class.watches = null;
+
+          _Class.dirtyWatches = null;
+
+          function _Class() {
+            this.checkInputValid = __bind(this.checkInputValid, this);
+            this.removeWatch = __bind(this.removeWatch, this);
+            this.addWatch = __bind(this.addWatch, this);
+            this.addAllWatches = __bind(this.addAllWatches, this);
+            this.findParentElements = __bind(this.findParentElements, this);
+            this.watchForm = __bind(this.watchForm, this);
+            this.elements = {};
+            this.watches = {};
+            this.dirtyWatches = {};
+            $timeout((function(_this) {
+              return function() {
+                _this.findParentElements();
+                _this.addAllWatches();
+                return _this.watchForm();
+              };
+            })(this), 0);
+          }
+
+          _Class.prototype.watchForm = function() {
+            return $scope.$watch("" + $attrs.name, (function(_this) {
+              return function(newValue, oldValue) {
+                var hasNew, input, name;
+                for (name in newValue) {
+                  if (!__hasProp.call(newValue, name)) continue;
+                  input = newValue[name];
+                  if (!(!name.match(/^\$/))) {
+                    continue;
+                  }
+                  if (!oldValue[name]) {
+                    _this.addWatch(name, input);
+                  }
+                  hasNew = true;
+                }
+                for (name in oldValue) {
+                  if (!__hasProp.call(oldValue, name)) continue;
+                  input = oldValue[name];
+                  if (!name.match(/^\$/)) {
+                    if (!newValue[name]) {
+                      _this.removeWatch(name);
+                    }
+                  }
+                }
+                if (hasNew) {
+                  return _this.findParentElements();
+                }
+              };
+            })(this), true);
+          };
+
+          _Class.prototype.findParentElements = function() {
+            var candidate, element, _i, _len, _ref, _results;
+            _ref = $element[0].elements;
+            _results = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              element = _ref[_i];
+              if (!(element.type !== 'hidden' && !this.elements[element.name])) {
+                continue;
+              }
+              candidate = angular.element(element);
+              while (true) {
+                candidate = candidate.parent();
+                if (candidate.hasClass('form-group') || !candidate || candidate.length === 0) {
+                  break;
+                }
+              }
+              if (candidate) {
+                _results.push(this.elements[element.name] = candidate);
+              } else {
+                _results.push(void 0);
+              }
+            }
+            return _results;
+          };
+
+          _Class.prototype.addAllWatches = function() {
+            var formCtrl, input, name, _results;
+            formCtrl = controllers[1];
+            _results = [];
+            for (name in formCtrl) {
+              if (!__hasProp.call(formCtrl, name)) continue;
+              input = formCtrl[name];
+              if (!name.match(/^\$/)) {
+                _results.push(this.addWatch(name, input));
+              }
+            }
+            return _results;
+          };
+
+          _Class.prototype.addWatch = function(name, input) {
+            if (!this.watches[name]) {
+              this.watches[name] = $scope.$watch("" + $attrs.name + "['" + name + "'].$invalid", (function(_this) {
+                return function() {
+                  return _this.checkInputValid(name);
+                };
+              })(this));
+              this.dirtyWatches[name] = $scope.$watch("" + $attrs.name + "['" + name + "'].$dirty", (function(_this) {
+                return function() {
+                  return _this.checkInputValid(name);
+                };
+              })(this));
+              return this.checkInputValid(name);
+            }
+          };
+
+          _Class.prototype.removeWatch = function(name) {
+            if (this.watches[name]) {
+              this.watches[name]();
+            }
+            if (this.dirtyWatches[name]) {
+              this.dirtyWatches[name]();
+            }
+            delete this.watches[name];
+            return delete this.dirtyWatches[name];
+          };
+
+          _Class.prototype.checkInputValid = function(name) {
+            var element;
+            element = $scope[$attrs.name][name];
+            if (element && element.$invalid && element.$dirty) {
+              if (this.elements[name]) {
+                this.elements[name].addClass("has-error");
+              }
+              if (this.elements[name]) {
+                return this.elements[name].addClass("has-feedback");
+              }
+            } else {
+              if (this.elements[name]) {
+                this.elements[name].removeClass("has-error");
+              }
+              if (this.elements[name]) {
+                return this.elements[name].removeClass("has-feedback");
+              }
+            }
+          };
+
+          return _Class;
+
+        })());
+      },
       controller: function($scope, $element, $attrs, $http) {
         var CrdsAjaxFormController;
         return new (CrdsAjaxFormController = (function() {
-          CrdsAjaxFormController.successHandler;
+          CrdsAjaxFormController.successHandler = null;
 
           function CrdsAjaxFormController() {
             this.registerSuccess = __bind(this.registerSuccess, this);
             var clearDirty, clearError, errorFields, forceDirty, input, showError;
+            this.FORM_SUCCESS_MESSAGE = "form.success";
+            this.VALIDATION_ERROR_MESSAGE = "form.validation.error";
+            this.SERVER_ERROR_MESSAGE = "form.server.error";
             errorFields = [];
+            this.form = $scope[$attrs.name];
             this.hidden = (function() {
               var _i, _len, _ref, _results;
               _ref = $element[0].elements;
@@ -109,7 +262,7 @@
             };
             $element.bind('submit', (function(_this) {
               return function(event) {
-                var field, fields, form, _i, _j, _len, _len1, _ref;
+                var errorMessage, field, fields, form, httpConfig, _i, _j, _len, _len1, _ref;
                 event.stopPropagation();
                 event.preventDefault();
                 form = $scope[$attrs.name];
@@ -126,22 +279,30 @@
                     input = _ref[_j];
                     fields[input.name] = input.value;
                   }
-                  $http({
+                  httpConfig = {
                     method: $attrs.method,
                     url: $attrs.action,
                     data: fields
-                  }).success(function(data, status, headers, config) {
+                  };
+                  if (!$attrs.enctype || $attrs.enctype === 'application/x-www-form-urlencoded') {
+                    httpConfig.transformRequest = _this.formUrlEncodedRequest;
+                  } else if ($attrs.enctype !== 'application/json') {
+                    errorMessage = "Ajax Form does not support " + $attrs.enctype + " encoded forms";
+                    console.log(errorMessage);
+                    return false;
+                  }
+                  $http(httpConfig).success(function(data, status, headers, config) {
                     if (_this.successMessage) {
                       _this.successMessage();
                       $element.remove();
                     } else {
-                      $scope.$emit('notify.success', "Success!");
+                      $scope.$emit('notify.success', _this.FORM_SUCCESS_MESSAGE);
                     }
                     form.$setPristine();
                   }).error(function(data, status, headers, config) {
                     var error, _k, _len2;
                     if (status === 400) {
-                      $scope.$emit('notify.warning', "Server-side validation failed");
+                      $scope.$emit('notify.error', _this.VALIDATION_ERROR_MESSAGE);
                       if (data) {
                         for (_k = 0, _len2 = data.length; _k < _len2; _k++) {
                           error = data[_k];
@@ -149,11 +310,11 @@
                         }
                       }
                     } else {
-                      $scope.$emit('notify.error', "An unknown error occurred during form submission");
+                      $scope.$emit('notify.error', _this.SERVER_ERROR_MESSAGE);
                     }
                   });
                 } else {
-                  $scope.$emit('notify.warning', "Client-side validation failed");
+                  $scope.$emit('notify.error', _this.VALIDATION_ERROR_MESSAGE);
                   forceDirty(form);
                 }
                 return false;
@@ -164,6 +325,31 @@
 
           CrdsAjaxFormController.prototype.registerSuccess = function(successMessage) {
             return this.successMessage = successMessage;
+          };
+
+          CrdsAjaxFormController.prototype.formUrlEncodedRequest = function(data, getHeaders) {
+            var headers, key, params, value;
+            headers = getHeaders();
+            headers['Content-Type'] = "application/x-www-form-urlencoded; charset=utf-8";
+            if (angular.isObject(data)) {
+              params = (function() {
+                var _results;
+                _results = [];
+                for (key in data) {
+                  if (!__hasProp.call(data, key)) continue;
+                  value = data[key];
+                  _results.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+                }
+                return _results;
+              })();
+              return params.join('&').replace(/%20/g, '+');
+            } else {
+              if (data !== null) {
+                return data.toString();
+              } else {
+                return '';
+              }
+            }
           };
 
           return CrdsAjaxFormController;
