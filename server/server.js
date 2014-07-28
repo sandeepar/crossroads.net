7325f900ff5d0dc70038ddacc73dff53b115afd0
aravind666 <aravind.udayashankara@gmail.com>
2014-07-28 12:17:03 +0530

    Made changes to server bootstrap MIL-336

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app, dotenv, express, passport;
 express = require('express');
 bodyParser = require('body-parser');
 expressValidator = require('express-validator');
+path = require('path');
 
 app = express();
 app.use(bodyParser());
