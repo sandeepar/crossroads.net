6c0282c7e62ab5276ae2e856425c101846d6d1aa
IngageMaddoxT <tony.maddox@ingagepartners.com>
2014-07-30 12:37:34 -0400

    added body parser to correct login failure

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,6 +2,7 @@ var app, apiProxy, bodyParser, express, session, passport, RedisStore, refreshTo
 
 apiProxy = require('./api-proxy');
 express = require('express');
+bodyParser = require('body-parser');
 passport = require('passport');
 cookieParser = require('cookie-parser');
 session = require('express-session');
@@ -9,19 +10,28 @@ RedisStore = require('connect-redis')(session);
 refreshToken = require('./refresh-token');
 
 module.exports = function(app) {
+  app.enable('trust proxy');
+
   app.use(cookieParser());
   app.use(session({
-    store: new RedisStore({ url: process.env.REDISCLOUD_URL }),
-    key: "crossroads.sid",
-    secret: 'secret',
-    cookie: { path: '/', httpOnly: true, maxAge: null },
-    resave: true,
-    saveUninitialized: true
+      store: new RedisStore({ url: process.env.REDISCLOUD_URL }),
+      key: "crossroads.sid",
+      secret: 'secret',
+      cookie: { path: '/', httpOnly: true, maxAge: null }
   }));
+
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(refreshToken());
+
   app.use('/api', apiProxy('https://my.crossroads.net/'));
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  app.use(expressValidator([]));
+
+
   app.use(express.static(__dirname + "/../../generated"));
   if (process.env.NODE_ENV === 'development') {
     app.use(express.static(__dirname + "/../../.tmp"));
