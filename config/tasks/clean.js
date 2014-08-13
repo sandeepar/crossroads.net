5fcc1eccaa44e74a8a181f4c6b5f71ed4a318124
Jim Anders <github@janders223.com>
2014-08-13 16:39:05 -0400

    Switch to del for task blocking

--- config/tasks/clean.js
+++ config/tasks/clean.js
@@ -1,5 +1,7 @@
+var del = require('del');
+
 module.exports = function(gulp, $) {
-  return gulp.task("clean", function() {
-    gulp.src("generated/{js,css}/**/*.*").pipe($.clean());
+  return gulp.task("clean", function(cb) {
+    del(["generated/{js,css}/**/*.*", ".tmp/**/*"], cb)
   });
 };
