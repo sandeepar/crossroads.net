f88794e792b7820db1c7ab09464d587d0a8a754e
Jim Anders <github@janders223.com>
2014-08-15 15:45:02 -0400

    Fix sass task to include module.scss

--- config/tasks/sass.js
+++ config/tasks/sass.js
@@ -2,7 +2,7 @@ var browserSync = require('browser-sync');
 
 module.exports = function(gulp, opts, $) {
   return gulp.task('sass', ['clean:css'], function() {
-    return gulp.src(["app/scss/main.scss", "modules/**/css/module.scss"])
+    return gulp.src(["app/scss/main.scss", "app/js/**/css/module.scss"])
       .pipe($.concat('app.scss'))
       .pipe($.rubySass({ sourcemap: false }).on("error", $.util.log))
       .pipe($.autoprefixer("last 2 versions", "Firefox >= 20", { cascade: true }))
