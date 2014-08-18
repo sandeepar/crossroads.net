9cfbfac832555ada4e8c63a0c4ac513b6003d1d8
sandeepar <sandeep.setty@costrategix.com>
2014-08-18 17:47:39 +0530

    MIL-338 outdated browser plugin

---
+++ app/js/crossroads.net/services/outdated_browser.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('crossroads');
+
+angular.element(document).ready(function(){
+
+       var $buoop = {}; 
+       $buoop.ol = window.onload; 
+       window.onload=function(){ 
+       try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
+       var e = document.createElement('script'); 
+       e.setAttribute('type', 'text/javascript'); 
+       e.setAttribute('src', '//browser-update.org/update.js'); 
+       document.body.appendChild(e); 
+      }; 
+});
+
